612524e103f655749f6da98015c13538e70a28b5
AnaniasLima <ananias.lima@hotmail.com>
2021-04-27 00:47:03 -0300

    Fazendo consulta pix no servidor de producao

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ if ( process.env.NODE_ENV !== 'production' ) {
 }
 
 const express = require('express');
-const GNRequest = require('./apis/gerencianet')
+const GNRequest = require('./apis/gerencianet');
 
 const app = express();
 
@@ -26,15 +26,18 @@ app.get('/', async (req, res) => {
         solicitacaoPagador: 'Cobrança de teste.'
     };
 
-    const cobResponse = await reqGN.post('v2/cob', dataCob);
-    
-    // res.send(cobResponse.data);
+   // const cobResponse = await reqGN.post('/v2/cob', dataCob);
+   //res.send(cobResponse.data);
 
-    const qrcodeResponse = await reqGN.get(`/V2/loc/${cobResponse.data.loc.id}/qrcode`);
-    res.render('qrcode', {qrcodeImage: qrcodeResponse.data.imagemQrcode});
+  //const qrcodeResponse = await reqGN.get(`/V2/loc/${cobResponse.data.loc.id}/qrcode`);
 
-//    const qrcodeResponse = await reqGN.get('/V2/pix?inicio=2020-10-22T16:01:35Z&2021-10-22T16:01:35Z&txid=123012345678901234');
-  //  res.send(qrcodeResponse.data);
+  //  res.render('qrcode', {qrcodeImage: qrcodeResponse.data.imagemQrcode});
+
+//  const pixResponse = await reqGN.get('/v2/pix?inicio=2020-10-22T16:01:35Z&fim=2021-04-22T16:01:35Z&txid=ananias123012345678901234');
+  const pixResponse = await reqGN.get(`/v2/pix/E18236120202103060137s0923240ZGT`);
+  //pix/E18236120202103060137s0923240ZGT
+   res.send(pixResponse.data);
+   //res.send('Legal');
 
 
 });
